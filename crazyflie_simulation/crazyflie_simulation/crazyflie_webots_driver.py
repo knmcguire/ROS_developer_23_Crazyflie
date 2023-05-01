12c83fcd9fe9a2d82c95912690129097d97049eb
knmcguire <kimberly@bitcraze.io>
2023-05-01 14:30:21 +0200

    Added all files necessary for the crazyflie sim

--- crazyflie_simulation/crazyflie_simulation/crazyflie_webots_driver.py
+++ crazyflie_simulation/crazyflie_simulation/crazyflie_webots_driver.py
@@ -1,6 +1,10 @@
-def main():
-    print('Hi from crazyflie_simulation.')
+import rclpy
 
+class CrazyflieDriver:
+    def init(self, webots_node, properties):
+        self.robot = webots_node.robot
+        rclpy.init(args=None)
+        self.node = rclpy.create_node('crazyflie_webots_driver')
 
-if __name__ == '__main__':
-    main()
+    def step(self):
+        rclpy.spin_once(self.node, timeout_sec=0)
